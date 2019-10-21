c1ac275fecfe51f085fedd128ac3cc6ceee8fce3
mayaams <mayaams27@gmail.com>
2019-10-21 21:42:04 +0700

    add ui detail barang

---
+++ elang/application/views/detailBarang.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<title>ELANG</title>
+  	<meta charset="UTF-8">
+  	<meta name="description" content="WebUni Education Template">
+  	<meta name="keywords" content="webuni, education, creative, html">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<!-- Favicon -->
+  	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+  	<!-- Google Fonts -->
+  	<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+  	<!-- Stylesheets -->
+  	<link rel="stylesheet" href="assets/bootstrap.min.css">
+  	<link rel="stylesheet" href="assets/font-awesome.min.css">
+  	<link rel="stylesheet" href="assets/owl.carousel.css">
+  	<link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/css/maya.css">
+
+  	<!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+      <!-- Popper JS -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+      <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  </head>
+  <style>
+    .i-size{
+      width: 20px;
+      height: 20px;
+    }
+    .ktm-size{
+      height: 250px;
+    }
+
+  </style>
+  <body>
+    <!-- Page Preloder -->
+  	<div id="preloder">
+  		<div class="loader"></div>
+  	</div>
+
+  	<!-- Header section
+  	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <a class="navbar-brand" href="#"> <img src="https://portal.merauke.go.id/files/momo.png" alt="" style="width:25px; height:25px;margin-right:20px;"><b>E-LANG</b></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <form class="my-2 my-lg-0">
+              <div class="dropdown" style="margin-left:-90px;margin-bottom: -37px;">
+                  <button type="button" class="btn btn-dark dropdown-toggle " data-toggle="modal" data-target="#exampleModa3">shahnaz</button>
+                  <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Edit Profil</a>
+                      <a class="dropdown-item" href="#">Keluar</a>
+                  </div>
+              </div>
+  		   <button type="button" class="btn btn-dark">FAQ</button>
+            </form>
+          </div>
+      </nav>
+  	 Header section end -->
+    <div class="hero-section set-bg" data-setbg="img/bg-1.png">
+      <div class="container pt-5 col-7 text-body" style="font-family: 'Roboto', sans-serif;">
+        <div class="row p-4 rounded-lg bg-white" style="opacity: 0.75;">
+          <div class="col-12 border border-warning">
+            <div class="row">
+              <div class="col-4 border border-danger">
+                <div class="pl-3 border border-dark">
+                  <img src="img/ktm_c.jpg" class="ktm-size">
+                </div>
+                <div>
+                  <a style="font-size : 13px;">Bagikan ke</a><br>
+                  <span><img src="img/icon/ig.svg" class="i-size"><img src="img/icon/fb.svg" class="i-size"><img src="img/icon/twitter.svg" class="i-size"><img src="img/icon/g+.svg" class="i-size"></span>
+                </div>
+              </div>
+              <div class="col-8 border border-secondary">
+                <div>
+                  <a class="font-weight-bold" style="font-size : 18px;">Kartu Tanda Mahasiswa</a><br>
+                </div>
+                <div class="mt-1">
+                  <span> <img src="img/icon/map.svg" class="i-size"><a style="font-size : 13px;">GKU KU1.03.03</a></span><br>
+                  <a style="font-size : 13px;">Kategori: <span>Kartu</span></a><br>
+                  <a style="font-size : 13px;">Status  : <span>HILANG</span></a><br>
+               </div>
+                <div class="mt-3">
+                  <a class="font-weight-bold" style="font-size : 14px;">DESKRIPSI</a><br>
+                  <a style="font-size : 13px;">Kehilangan KTM sekitar pukul 16:30 setelah kelas Kalkulus IIB. Kemungkinan lokasi hilang di bangku-bangku bagian belakang. Terdapat strap berwarna kuning dengan motif pisang.</a><br>
+               </div>
+               <div class="mt-3">
+                  <a class="font-weight-bold" style="font-size : 14px;">DETAIL</a><br>
+                  <div class="row border border-danger">
+                    <div class="col-8" style="font-size : 13px;">
+                      <a>Ciri-Ciri:</a><br>
+                      <li>Tempat kartu berwarna kuning</li>
+                      <li>Tidak terdapat coretan</li>
+                      <li>Masih menggunakan tinta nyata</li>
+                    </div>
+                    <div class="col-4" style="font-size : 13px;">
+                      <a>Hilang Tanggal</a><br>
+                      <a>2 Oktober 2019</a><br>
+                    </div>
+                  </div>
+              </div>
+                <button type="button" class="btn btn-primary mt-4 btn-md btn-block">SAYA MENEMUKANNYA</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      <!-- </div> -->
+    </div>
+    </div>
+
+    <!--====== Javascripts & Jquery ======-->
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  	<script src="assets/js/mixitup.min.js"></script>
+  	<script src="assets/js/circle-progress.min.js"></script>
+  	<script src="assets/js/owl.carousel.min.js"></script>
+  	<script src="assets/js/main.js"></script>
+    <!--======================================-->
+
+  </body>
+
+</html>
