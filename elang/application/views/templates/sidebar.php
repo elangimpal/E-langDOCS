ee28dc2c75f056a6d2d1a5ae9a26a3eedcbc9c38
shahnazsyifaa <shahnaz.nurasyifa@gmail.com>
2019-10-22 09:34:19 +0700

    update

--- elang/application/views/templates/sidebar.php
+++ elang/application/views/templates/sidebar.php
@@ -9,8 +9,9 @@
             
             <a href="#demo" class="btn" style="background-color:#142E3F; color:white; margin-left:-80px" data-toggle="collapse"><b>Dashboard</b></a>
             <div id="demo" class="collapse">
+                <a href="<?= base_url('index.php/Dashboard')?>">Dashboard</a>
                 <a href="<?= base_url('index.php/formDitemukan')?>">Menemukan Barang</a>
-                <a href="<?= base_url('index.php/formHilang')?>">Kehilangan Barang Barang</a>
+                <a href="<?= base_url('index.php/formHilang')?>">Kehilangan Barang</a>
             </div>
             <a href="<?= base_url('index.php/Notifikasi')?>">Notifikasi</a>
             <a href="<?= base_url('index.php/BarangHilang')?>">Barang Hilang</a>
