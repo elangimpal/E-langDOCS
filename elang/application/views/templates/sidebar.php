4e4cb4d3bb294e1e50d09191b4150c103f52826a
shahnazsyifaa <shahnaz.nurasyifa@gmail.com>
2019-11-05 07:09:06 +0700

    sidebar updated

--- elang/application/views/templates/sidebar.php
+++ elang/application/views/templates/sidebar.php
@@ -3,18 +3,20 @@
 <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/sidebar.css">
 <div class="sidenav">
             <div class="profile">
-                <h5>LAMAN PROFILE</h5>
+                <h5>E-LANG</h5>
             </div>
 
-            
-            <a href="#demo" class="btn" style="background-color:#142E3F; color:white; margin-left:-80px" data-toggle="collapse"><b>Dashboard</b></a>
-            <div id="demo" class="collapse">
-                <a href="<?= base_url('index.php/Dashboard')?>">Dashboard</a>
-                <a href="<?= base_url('index.php/formDitemukan')?>">Menemukan Barang</a>
-                <a href="<?= base_url('index.php/formHilang')?>">Kehilangan Barang</a>
+            <a href="<?= base_url('index.php/Dashboard')?>">Beranda</a>
+            <a href="#inputBarang" class="btn" style="background-color:#142E3F; margin-left:-70px" data-toggle="collapse">Input Barang</a>
+            <div id="inputBarang" class="collapse">
+                <a href="<?= base_url('index.php/formDitemukan')?>" style="margin-left:30px">Menemukan Barang</a>
+                <a href="<?= base_url('index.php/formHilang')?>" style="margin-left:30px">Kehilangann Barang</a>
             </div>
             <a href="<?= base_url('index.php/Notifikasi')?>">Notifikasi</a>
-            <a href="<?= base_url('index.php/BarangHilang')?>">Barang Hilang</a>
-            <a href="<?= base_url('index.php/BarangDitemukan')?>">Barang Ditemukan</a>
+            <a href="#riwayat" class="btn" style="background-color:#142E3F;margin-left:-105px" data-toggle="collapse">Riwayat</a>
+            <div id="riwayat" class="collapse">
+                <a href="<?= base_url('index.php/BarangHilang')?>" style="margin-left:30px">Riwayat Barang Hilang</a>
+                <a href="<?= base_url('index.php/BarangDitemukan')?>" style="margin-left:30px">Riwayat Barang Ditemukan</a>
+            </div>
             <a href="<?= base_url('index.php/Profile_controller')?>">Pengaturan</a>
         </div>
