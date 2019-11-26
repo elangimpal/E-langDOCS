2cf82e4dbcf2e09d472dcb9d806e6e336b791e42
ciaranrdnr <ciaranurdenara@gmail.com>
2019-11-26 12:34:46 +0700

    selasa

--- elang/application/controllers/Login_controller.php
+++ elang/application/controllers/Login_controller.php
@@ -1,44 +1,57 @@
 <?php
-class Login_controller extends CI_Controller{
+class Login_controller extends CI_Controller
+{
 
-	function __construct(){
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->model('LoginModel');
+		// Load form helper library
+		$this->load->helper('form');
 
+		// Load form validation library
+		$this->load->library('form_validation');
+
+		// Load session library
+		$this->load->library('session');
 	}
 
-	function index(){
+	function index()
+	{
 		$this->load->view('Home');
 	}
 
-	function aksi_login(){
+	function aksi_login()
+	{
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$where = array(
 			'username' => $username,
-			'password' => md5($password)
-			);
-		$cek = $this->LoginModel->cek_login("user",$where)->num_rows();
-		if($cek > 0){
+			//	'password' => md5($password)  //di mySQL blm di encrypt
+			'password' => $password  //di mySQL blm di encrypt
+
+		);
+		$cek = $this->LoginModel->cek_login("user", $where)->num_rows();
+		if ($cek > 0) {
 
 			$data_session = array(
 				'nama' => $username,
 				'status' => "login"
-				);
+			);
 
 			$this->session->set_userdata($data_session);
 
 			redirect(base_url("index.php/Dashboard_controller"));
-
-		}else{
+		} else {
 			// redirect(base_url("index.php/Home"));
-			echo $username," ";
-			echo md5($password)," ";
+			echo $username, " ";
+			echo md5($password), " ";
 			echo "Username dan password salah !";
 		}
 	}
 
-	function logout(){
+	function logout()
+	{
 		$this->session->sess_destroy();
 		redirect(base_url('login'));
 	}
